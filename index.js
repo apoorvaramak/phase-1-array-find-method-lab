f1bf4e1c42de699007b114a04a4b96ac49fa4115
Apoorva Ramakrishnan <apoorvar@comcast.net>
2021-06-29 09:25:38 -0500

    commit tree b85db9a96afda22a64a84932da680a9227b8d54f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function isAWin(element){
+    return(element.result === 'W');
+}
 
+function superbowlWin(arr){
+    if(arr.find(isAWin) === undefined){
+        return arr.find(isAWin)
+    }
+    return arr.find(isAWin).year;
+    
+}
